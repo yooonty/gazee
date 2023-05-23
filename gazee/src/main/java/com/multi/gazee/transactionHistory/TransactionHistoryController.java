b4a20a6e403674311d1ced4f848ef21ed3d6dd63
Yoonjung Yim <yoonjung623@naver.com>
2023-05-23 17:53:59 +0900

    Feat : 잔액 조회(BE)

---
+++ gazee/src/main/java/com/multi/gazee/transactionHistory/TransactionHistoryController.java
@@ -0,0 +1,22 @@
+package com.multi.gazee.transactionHistory;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TransactionHistoryController {
+	@Autowired
+	TransactionHistoryDAO dao;
+	
+	@RequestMapping("pay/record")
+	@ResponseBody
+	public String checkBalance(HttpSession session) {
+		session.setAttribute("id", "testuser");
+		String balance = String.valueOf(dao.checkBalance(String.valueOf(session.getAttribute("id"))));
+		return balance;
+	}
+}
---
+++ gazee/src/main/java/com/multi/gazee/transactionHistory/TransactionHistoryDAO.java
@@ -0,0 +1,24 @@
+package com.multi.gazee.transactionHistory;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionHistoryDAO {
+	
+	@Autowired
+	SqlSessionTemplate my;
+	
+	public int checkBalance(String id) {
+		int balance;
+
+		try {
+			balance = my.selectOne("history.select", id);
+		} catch (Exception e) {
+			balance = 0;
+		}
+		
+		return balance;
+	}
+}
