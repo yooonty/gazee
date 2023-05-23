5d3991de994413d44fd05f91f498d5925b58c262
JJuu <jyj120572@gmail.com>
2023-05-23 17:49:51 +0900

    Feat : 웹소켓 관련 기능 - 웹소켓 Config Brocker 설정 - 메세지 OutputVO, Controller

---
+++ gazee/src/main/java/com/multi/gazee/chat/WebSocketController.java
@@ -0,0 +1,35 @@
+package com.multi.gazee.chat;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+import org.bson.BsonTimestamp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebSocketController {
+	
+	@Autowired
+	ChatMessageDAO dao;
+	
+	@MessageMapping("/chat/{roomId}")
+	@SendTo("/topic/{roomId}")
+	public ChatOutputMessageVO chatMessage(ChatMessageVO message, @DestinationVariable String roomId) {
+		BsonTimestamp timestamp = new BsonTimestamp((int) (System.currentTimeMillis() / 1000), 0);
+		message.setDate(timestamp);
+		message.setRoomId(Integer.parseInt(roomId));
+		dao.insert(message);
+		ChatOutputMessageVO output = new ChatOutputMessageVO();
+		output.setRoomId(roomId);
+		output.setSender(message.getSender());
+		output.setContent(message.getContent());
+		SimpleDateFormat simple2 = new SimpleDateFormat("HH:mm");
+		output.setTime(simple2.format(new Timestamp(System.currentTimeMillis())));
+		return output;
+	}
+
+}
