47504de359abfb5651e6201e3eae3b9154c0d6cf
zeehoonee <qlscmgns@gmail.com>
2023-05-24 10:12:17 +0900

    Feat : ReportDAO

---
+++ gazee/src/main/java/com/multi/gazee/customerService/report/ReportDAO.java
@@ -0,0 +1,76 @@
+package com.multi.gazee.customerService;
+
+import com.multi.gazee.paging.PageVO;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Component
+public class ReportDAO {
+    @Autowired
+    SqlSessionTemplate my;
+    
+    public int reportRegister(ReportVO bag) {
+        int result = my.insert("report.register", bag);
+        return result;
+    }
+    
+    public int replyRegister(ReportVO bag) {
+        int result = my.insert("report.reply", bag);
+        return result;
+    }
+    
+    public List<ReportVO> needReplyList(PageVO vo){
+        List<ReportVO> needReplyList = my.selectList("report.needReply", vo);
+        return needReplyList;
+    }
+    
+    public void delete(ReportVO bag) {
+        my.delete("report.del", bag);
+    }
+    
+    public void update(ReportVO bag) {
+        my.delete("report.up", bag);
+    }
+    
+    public ReportVO one(int no) {
+        ReportVO bag = my.selectOne("report.one",no);
+        my.update("report.viewUpdate", no);
+        return bag;
+    }
+    public List<ReportVO> nonPagedList(){
+        List<ReportVO> list = my.selectList("report.nonPagedList");
+        return list;
+    }
+    public List<ReportVO> list(PageVO vo){
+        List<ReportVO> list = my.selectList("report.all", vo);
+        return list;
+    }
+    
+    public List<ReportVO> category(HashMap<String, Object> map){
+        List<ReportVO> category = my.selectList("report.category", map);
+        return category;
+    }
+    
+    
+    public List<ReportVO> search(HashMap<String, Object> map){
+        List<ReportVO> search = my.selectList("report.searchAll", map);
+        System.out.println(search.size());
+        return search;
+    }
+    
+    public int count() {
+        return my.selectOne("report.count");
+    }
+    
+    public int countCategory(String category1) {
+        return my.selectOne("report.countCategory", category1);
+    }
+    
+    public int countSearch(String search1) {
+        return my.selectOne("report.countSearch", search1);
+    }
+}
