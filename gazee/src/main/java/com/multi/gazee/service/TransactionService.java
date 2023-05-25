09d419b373b3bf0c827deac05b4423fd942a143c
Yoonjung Yim <yoonjung623@naver.com>
2023-05-25 16:41:14 +0900

    Feat : 가지머니 관련 서비스 생성
    - 일련번호 생성
    - 서버시간 가져오기
    - 거래 VO -> TransactionHistoryVO 변환 후 Insert

---
+++ gazee/src/main/java/com/multi/gazee/service/TransactionService.java
@@ -0,0 +1,24 @@
+package com.multi.gazee.service;
+
+import java.sql.Timestamp;
+
+import com.multi.gazee.charge.ChargeVO;
+import com.multi.gazee.member.MemberVO;
+import com.multi.gazee.order.OrderVO;
+import com.multi.gazee.set.SetVO;
+import com.multi.gazee.transactionHistory.TransactionHistoryVO;
+import com.multi.gazee.withdraw.WithdrawVO;
+
+public interface TransactionService {
+	String makeIdentifier(String transactionType, MemberVO memberVO, Timestamp transactionTime);
+	
+	Timestamp getTransactionTime();
+	
+	int chargeToTransactionHistory(ChargeVO chargeVO, String id);
+
+	int withdrawToTransactionHistory(WithdrawVO woithdrawVO, int balance);
+	
+	int orderToTransactionHistory(OrderVO orderVO, int paid_amount, int balance);
+	
+	int setToTransactionHistory(SetVO setVO, String id);
+}
