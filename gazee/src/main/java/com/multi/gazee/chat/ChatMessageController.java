20d0808f55cf8f1d2c46d8087949687f3ac1a718
JJuu <jyj120572@gmail.com>
2023-05-26 15:04:08 +0900

    Feat : ChatMessageController - Service 분리

--- gazee/src/main/java/com/multi/gazee/chat/ChatMessageController.java
+++ gazee/src/main/java/com/multi/gazee/chat/ChatMessageController.java
@@ -1,11 +1,5 @@
 package com.multi.gazee.chat;
 
-import java.sql.Timestamp;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,54 +7,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.multi.gazee.service.ChatService;
+
 @Controller
 public class ChatMessageController {
 
 	@Autowired
-	ChatMessageDAO chatMessageDao;
+	ChatService ChatService;
 	
-	@Autowired
-	ChatDAO chatDao;
-	
+	/* 채팅메세지 내역 */
 	@RequestMapping("chat/chatMessageList")
 	@ResponseBody
 	public List<ChatOutputMessageVO> list(int roomId) {
-		List<ChatOutputMessageVO> arrList = chatMessageDao.list(roomId);
-		return arrList;
+		return ChatService.chatMessageList(roomId);
 	}
 	
+	/* 마지막 채팅 메세지 */
 	@RequestMapping("chat/chatLastMessage")
 	@ResponseBody
-	public ChatMessageVO lastMeesage(int roomId) {
-		ChatMessageVO bag = chatMessageDao.lastMessageList(roomId);
-		return bag;
+	public ChatMessageVO chatLastMessage(int roomId) {
+		return ChatService.chatLastMessage(roomId);
 	}
 	
+	/* 마지막 채팅 시간 업데이트 */
 	@RequestMapping("chat/lastMessageTimeUpdate")
 	@ResponseBody
 	public int lastMessageTimeUpdate(int roomId) {
-		ChatMessageVO bag = chatMessageDao.lastMessageList(roomId);
-		if (bag != null) {
-			ChatVO bag2 = new ChatVO();
-			Date date = new Date(bag.getDate().getTime() * 1000L);
-			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			String timestamp = format.format(date);
-			try {
-				Date date2 = format.parse(timestamp);
-				Timestamp timestamp2 = new Timestamp(date2.getTime());
-				Calendar calendar = Calendar.getInstance();
-				calendar.setTime(timestamp2);
-				calendar.add(Calendar.HOUR_OF_DAY, 9);
-				Timestamp updatedTime = new Timestamp(calendar.getTimeInMillis());
-				bag2.setRoomId(roomId);
-				bag2.setLastMessageDate(updatedTime);
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-			int result = chatDao.lastMessageTimeUpdate(bag2);
-			return result;
-		} else {
-			return 0;
-		}
+		return ChatService.lastMessageTimeUpdate(roomId);
 	}
+
 }
