80854be3759377ad378b732d16cb5dee8b9ac4e8
Yoonjung Yim <yoonjung623@naver.com>
2023-05-26 14:30:07 +0900

    Feat : Service DAO->Controller로 이동, 응답코드 추가

--- gazee/src/main/java/com/multi/gazee/charge/ChargeController.java
+++ gazee/src/main/java/com/multi/gazee/charge/ChargeController.java
@@ -1,17 +1,18 @@
 package com.multi.gazee.charge;
 
+import java.sql.Timestamp;
+
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.multi.gazee.member.MemberDAO;
 import com.multi.gazee.member.MemberVO;
+import com.multi.gazee.service.TransactionService;
 
 @Controller
 public class ChargeController {
@@ -22,14 +23,34 @@ public class ChargeController {
 	@Autowired
 	MemberDAO memberDAO;
 	
+	@Autowired
+	TransactionService transactionService;
+	
 	@RequestMapping(value="pay/charge", method = {RequestMethod.POST})
-	public void payCharge(ChargeVO charge, HttpSession session) {
-		System.out.println("charge 도달");
+	public void payCharge(ChargeVO charge, HttpSession session, HttpServletResponse response) {
+		//1. 멤버 객체 가져오기
 		String memberId = String.valueOf(session.getAttribute("id"));
 		MemberVO member = memberDAO.selectOne(memberId);
 		charge.setMemberId(memberId);
-		
-		chargeDAO.charge(charge, member);
+		//2. 거래 시간 가져오기 
+		Timestamp transactionTime = transactionService.getTransactionTime();
+		charge.setTransactionTime(transactionTime);
+		//3. 거래 일련번호 생성
+		String identifier = transactionService.makeIdentifier("c", member, transactionTime);
+		charge.setTransactionId(identifier);
+		//4. 충전 테이블 삽입
+		int chargeResult = chargeDAO.charge(charge);
+		int transactionResult = 0;
+		//5. TransactionHistory 테이블 삽입
+		if (chargeResult==1) {
+			transactionResult = transactionService.chargeToTransactionHistory(charge, member.getId());
+		}
+		//6. 응답 코드 추가
+		if (transactionResult==1) {
+			response.setStatus(HttpServletResponse.SC_OK);
+		}else {
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
 	}
 	
 	
