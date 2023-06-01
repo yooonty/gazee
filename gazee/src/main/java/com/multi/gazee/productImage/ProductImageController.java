ea6014fc5162d2232a339e6d5b38445b74118c71
DESKTOP-104DH1D\Administrator <cyj8502@naver.com>
2023-06-02 02:21:13 +0900

    Feat : 상품 이미지 등록 구현(BE) -multipart file multiple이미지 등록 성공

--- gazee/src/main/java/com/multi/gazee/productImage/ProductImageController.java
+++ gazee/src/main/java/com/multi/gazee/productImage/ProductImageController.java
@@ -15,14 +15,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.multi.gazee.product.ProductDAO;
 import com.multi.gazee.product.ProductVO;
+import com.multi.gazee.productLikes.ProductLikesDAO;
+import com.multi.gazee.productLikes.ProductLikesVO;
+import com.multi.gazee.service.ProductImageService;
 
 @Controller
 public class ProductImageController {
 	
 	@Autowired
 	ProductImageDAO dao2;
-	
+	@Autowired
+	ProductLikesDAO like;
+	@Autowired
+	ProductDAO dao;
 //	@RequestMapping("product/detail")
 //	public void productImage(Model model,int productId) {
 //		ProductImageVO bag2 = dao2.productImage(productId);
@@ -32,16 +39,18 @@ public class ProductImageController {
 	
 	@Autowired
 	private ProductImageService s3Service;
+	@Autowired
+	ProductImageDAO productImgdao;
 
 	// 파일 업로드 처리
 	@RequestMapping(value = "product/uploadMultipleFile", method = RequestMethod.POST)
-	public @ResponseBody String uploadMultipleFileHandler(@RequestParam("file") List<MultipartFile> multiFileList, ProductImageVO productImage, HttpSession session) {
+	public void uploadMultipleFileHandler(@RequestParam("file") List<MultipartFile> multiFileList, ProductImageVO productImage, HttpSession session) {
 	    StringBuilder message = new StringBuilder();
 	    int order = 0;
-	    
-	 // 상품 등록 후 자동 생성된 productId 가져오기
+
+	    // 상품 등록 후 자동 생성된 productId 가져오기
 	    int productId = (int)session.getAttribute("productId");
-	    
+
 	    for (MultipartFile file : multiFileList) {
 	        String originalFileName = file.getOriginalFilename();
 	        String uuidFileName = getUuidFileName(originalFileName);
@@ -56,17 +65,38 @@ public class ProductImageController {
 	            System.out.println("이건 뭐야" + res);
 	            message.append("https://gazee.product.image.s3.ap-northeast-2.amazonaws.com/").append(uuidFileName).append("\n");
 	        } catch (Exception e) {
-	            return "You failed to upload " + uuidFileName + " => " + e.getMessage();
+	            throw new RuntimeException("Failed to upload " + uuidFileName, e);
 	        }
 	        System.out.println("productImagecontroller호출" + productImage);
 	        dao2.productImageUpload(productImage);
 	    }
-	    
-	    return message.toString();
 	}
 
 	private static String getUuidFileName(String originalFileName) {
 		return UUID.randomUUID().toString() + "_" + originalFileName;
+	}
+	
+	@RequestMapping("product/S3ProductDelete")
+	@ResponseBody
+	public void	productImageDelete(int productId,HttpSession session, Model model, ProductVO bag, ProductLikesVO likebag) {
+		
+		List<ProductImageVO> productImgList = productImgdao.productImage(productId);
+		for (ProductImageVO productImg : productImgList ) {
+			String objectName= productImg.getProductImageName();
+		try {
+		    s3Service.deleteObject(objectName);
+		    System.out.format("Object %s has been deleted.\n", objectName);
+		} catch (Exception e) {
+		    e.printStackTrace();
+			} 
+		}
+
+		model.addAttribute("bag", bag);
+		
+		like.likeDelete(likebag);
+		productImgdao.productImageDelete(productId);
+		dao.productDelete(bag);
+		
 	}
 	
 }
