5b01019472ac7ae111b5e964f7ebb2568e558741
DESKTOP-R7JOQ18\Administrator <pjw03659@naver.com>
2023-06-03 17:55:19 +0900

    Feat : Report 이미지 업로드

--- gazee/src/main/java/com/multi/gazee/reportImg/ReportImgController.java
+++ gazee/src/main/java/com/multi/gazee/reportImg/ReportImgController.java
@@ -14,58 +14,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.SdkClientException;
+import com.multi.gazee.service.ReportImgService;
 
 @Controller
 public class ReportImgController {
 	
 	@Autowired
-	private reportAWSS3Biz s3Service;
+	reportAWSS3Biz s3Service;
 	@Autowired
 	ReportImgDAO dao;
+	@Autowired
+	ReportImgService service;
 	
 	// 파일 업로드 처리
 	@RequestMapping(value = "reportImg/reportUploadMultipleFile", method = RequestMethod.POST)
-	public @ResponseBody String uploadMultipleFileHandler(@RequestParam("file") List<MultipartFile> multiFileList, HttpSession session, ReportImgVO bag) {
-	    int reportId =(int)session.getAttribute("reportId");
-	    int order=0;
-		
-		StringBuilder message = new StringBuilder();
-	    for (MultipartFile file : multiFileList) {
-	        String originalFileName = file.getOriginalFilename();
-	        String uuidFileName = getUuidFileName(originalFileName);
-	        bag.setReportId(reportId);
-	        bag.setReportImgName(uuidFileName);
-	        order++;
-	        bag.setReportImgOrder(order);
-	        dao.reportImgUpload(bag);
-	        try {
-	            // ========= 서버에 파일 저장 ========= // 
-	            String res = s3Service.uploadObject(file, uuidFileName);
-	            System.out.println("이건 뭐야" + res);
-	            message.append("https://gazee.customerService.image.s3.ap-northeast-2.amazonaws.com/").append(uuidFileName).append("\n");
-	        } catch (Exception e) {
-	            return "You failed to upload " + uuidFileName + " => " + e.getMessage();
-	        }
-	    }
-	    return message.toString();
-	}
-
-	private static String getUuidFileName(String originalFileName) {
-		return UUID.randomUUID().toString() + "_" + originalFileName;
+	public @ResponseBody void uploadMultipleFileHandler(@RequestParam("file") List<MultipartFile> multiFileList, HttpSession session, ReportImgVO bag) {
+	    service.uploadMultipleFileHandler(multiFileList, session, bag);
 	}
 	
-	@RequestMapping("customerService/report/reportImgDelete")
+	@RequestMapping("report/reportImgDelete")
 	public void	reportImgDelete(int reportId) {
-		List<ReportImgVO> reportImgList = dao.reportImgList(reportId);
-		for (ReportImgVO reportImg : reportImgList ) {
-			String objectName= reportImg.getReportImgName();
-		try {
-		    s3Service.deleteObject(objectName);
-		    System.out.format("Object %s has been deleted.\n", objectName);
-		} catch (Exception e) {
-		    e.printStackTrace();
-			} 
-		}
-		dao.reportImgDelete(reportId);
+		service.reportImgDelete(reportId);
 	}
 }
