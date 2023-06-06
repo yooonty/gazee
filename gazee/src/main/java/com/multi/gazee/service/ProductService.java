8d1dcdc2e89d56ef66def4c169f57203c13896ca
Yoonjung Yim <yoonjung623@naver.com>
2023-06-07 00:22:16 +0900

    Feat : ProductService 누락된 메소드 추가

--- gazee/src/main/java/com/multi/gazee/service/ProductService.java
+++ gazee/src/main/java/com/multi/gazee/service/ProductService.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.ui.Model;
 
+import com.multi.gazee.product.PageVO;
 import com.multi.gazee.product.ProductVO;
 import com.multi.gazee.productLikes.ProductLikesVO;
+import com.multi.gazee.weka.WekaRecommendService;
 
 public interface ProductService {
 	void productDetail(Model model, int productId);
@@ -23,6 +25,8 @@ public interface ProductService {
 	
 	void productUpdate(ProductVO bag);
 	
+	void productDelete(ProductVO bag);
+	
 	void checkTemporaryProduct(Model model, ProductVO bag);
 	
 	void productLikes(ProductLikesVO bag);
@@ -31,5 +35,35 @@ public interface ProductService {
 	
 	void checkLikes(Model model, ProductLikesVO bag);
 
+	int sellTimeUpdate(int productId);
+	
+	int sellTimeDelete(int productId);
+	
+	ProductVO sellTimeCheck(int productId);
+	
+	String best(Model model) throws Exception;
 
+	String userBest(String memberId, Model model) throws Exception;
+	
+	String wekaBest(String memberId, WekaRecommendService wekaRecommendService, Model model) throws Exception;
+
+	void searchList(PageVO vo, String search, Model model);
+	
+	void productList(PageVO vo, String search, Model model);
+	
+	void categoryList(PageVO vo, String category, Model model);
+	
+	String productList2(PageVO vo, String category, Model model) throws Exception;
+	
+	void viewCount(int productId);
+	
+	void searchListOnSale(PageVO vo, String search, Model model);
+	
+	String productListOnSale(PageVO vo, String search, Model model) throws Exception;
+
+	void categoryListOnSale(PageVO vo, String category, Model model);
+	
+	String productListOnSale2(PageVO vo, String category, Model model) throws Exception;
+	
+	String checkSeller(int productId) throws Exception;
 }
